e064b1cce8be10f8e7848ad06181f4aa9dc5c73f
[Edwin Sutherland] <[edsland@gmail.com]>
2020-03-15 20:16:31 +0000

    git add

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 import yaml
+import json
 import requests
+from dnacentersdk import api
 
-#open cred file for 
-with open('cred.yaml') as f:
-    data = yaml.load(f, Loader=yaml.FullLoader)
+#open cred file for dnac connection establishment
+with open('cred.json') as json_file:
+    cred = json.load(json_file)
 
-print(data['dnac-server'])
+dnac = api.DNACenterAPI(base_url=cred['dnacurl'],
+username=cred['username'],password=cred['passwd'], verify=False)
+
+sites = dnac.sites.get_site()
+print (sites)
+
+def create_sites():
+    dnac.sites.create_site(site=site['sites']['area']['name'], type=area, )
